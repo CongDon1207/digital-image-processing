c0b9c48e6d29fb974452430ec143f65f144468f8
don <22133013@student.hcmute.edu.vn>
2025-11-13 22:44:52 +0000

    set up folder gui

---
+++ week2/gui/app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def run_gui() -> None:
+    root = tk.Tk()
+    root.title("Xu ly anh so")
+    root.geometry("1000x500")
+    root.resizable(True, True)
+    
+
+    root.mainloop()
